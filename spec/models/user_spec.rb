aef707ae1aeb183c361eb84e58e1c529307a38d8
tadanotomo <to.iijima@gmail.com>
2020-08-26 12:25:02 +0900

    Rspecの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it "" do
+    end
+    it "" do
+    end
+    it "" do
+    end
+    it "" do
+    end
+    it "" do
+    end
+    it "" do
+    end
+    it "" do
+    end
+  end
+end
