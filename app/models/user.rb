7e584b4d98acdd2ba8e6f9fde6385a4f15d3df00
tadanotomo <to.iijima@gmail.com>
2020-08-26 16:29:18 +0900

    Rubocopを使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
-  VALID_EMAIL_REGEX = /\A[0-9a-z+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/
-  VALID_NAME_REGEX  = /\A[ぁ-んァ-ン一-龥]+\z/
-  VALID_KANANAME_REGEX = /\A[ァ-ヶー－]+\z/
+  VALID_EMAIL_REGEX = /\A[0-9a-z+\-.]+@[a-z\d\-.]+\.[a-z]+\z/.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/.freeze
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  VALID_KANANAME_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
   validates :nickname, :email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana,
             :birthday, presence: true
-  validates :email, format: { with: VALID_EMAIL_REGEX , message: "is invalid", allow_blank: true },
-            uniqueness: { case_sensitive: false }
-  validates :password, format: { with: VALID_PASSWORD_REGEX , message: "is invalid"}
-  validates :last_name, :first_name, format: { with: VALID_NAME_REGEX, message: "is invalid" }
-  validates :last_name_kana, :first_name_kana, format: { with: VALID_KANANAME_REGEX, message: "is invalid" }
+  validates :email, format: { with: VALID_EMAIL_REGEX, message: 'is invalid', allow_blank: true },
+                    uniqueness: { case_sensitive: false }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid' }
+  validates :last_name, :first_name, format: { with: VALID_NAME_REGEX, message: 'is invalid' }
+  validates :last_name_kana, :first_name_kana, format: { with: VALID_KANANAME_REGEX, message: 'is invalid' }
 end
