0c9f54d69dc89335ce810d637886e1509c499c7c
tadanotomo <to.iijima@gmail.com>
2020-08-28 00:48:58 +0900

    商品詳細機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show]
 
   def new
     @item = Item.new
@@ -18,5 +19,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :sipping_id, :sipping_origin_id, :sipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
