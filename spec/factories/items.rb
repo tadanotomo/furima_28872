a6d565e914d960a643f7094876337ab577db317c
tadanotomo <to.iijima@gmail.com>
2020-08-27 18:51:48 +0900

    itemモデルの単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Commerce.product_name}
+    description {Faker::Lorem.sentence}
+    price { '5000' }
+    category_id { '0' }
+    sipping_id { '0' }
+    sipping_origin_id { '0' }
+    sipping_date_id { '0' }
+    status_id { '0' }
+
+    association :user
   end
 end
